ea13493d7de4b4bba550ed2de3fd7acc95b27c75
Frank Botos <frankbotos@hotmail.com>
2022-07-21 04:45:20 -0400

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,6 @@ const firebase = require("firebase");
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
